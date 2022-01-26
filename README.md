24746f64c27805588f1b55fac853e0c232756c8d
Артем Лобовский <alobovskiy@ad.local>
2022-01-26 17:24:00 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fusely UI Kit
+
+UI Kit of [fusely template](https://www.figma.com/community/file/1046774226276680836)
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  watch: false,
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: "html-loader"
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: false } },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name][ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/font/[name][ext]'
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'fusely UI Kit',
+      favicon: './src/favicon.png',
+      template: './src/demo.html',
+      filename: 'demo.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
