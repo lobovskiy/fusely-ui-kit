9c3800d574ea200175f9bf6ba605c15f3dcb0764
Artem <lobovskiy@yandex.ru>
2022-01-30 20:06:50 +0300

    add instructions of using color palette

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,12 @@
 // ======================= highlight.js =========================
 
 import hljs from 'highlight.js/lib/core';
+
 import xml from 'highlight.js/lib/languages/xml';
 hljs.registerLanguage('xml', xml);
+
+import scss from 'highlight.js/lib/languages/scss';
+hljs.registerLanguage('scss', scss);
 
 import 'highlight.js/styles/foundation.css';
 
